c5927675c37aadbd2a3437100b9d355179690b6a
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-02-17 23:24:36 +0530

    Added Test file

---
+++ test.py
@@ -0,0 +1,11 @@
+import random
+fl = open('data.csv','w+')
+fl.write('0,1,2,3,4,5')
+fl.write('\n')
+for i in range(100):
+    a = random.randint(0,10)
+    b = random.randint(0,10)
+    c = random.randint(0,10)
+    fl.write('{0},{1},{2},{3},{4},{5}'.format(i,a,b,c,a+b,a-b))
+    fl.write('\n')
+fl.close()
