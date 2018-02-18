aadbd530270e88ccf64715f541285e34eaca215d
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-02-18 12:37:10 +0530

    Added API Gateway and serverless example for that api gateway

--- handler.py
+++ handler.py
@@ -2,7 +2,9 @@ import json
 from main import generate_network
 
 def machinelearning_generate(event, context):
-    code = generate_network()
+    input_body = event["body"]
+    input_body = json.loads(input_body)
+    code = generate_network(**input_body)
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "code": code,
@@ -11,6 +13,10 @@ def machinelearning_generate(event, context):
 
     response = {
         "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
+            "Access-Control-Allow-Credentials" : True  #// Required for cookies, authorization headers with HTTPS
+        },
         "body": json.dumps(body)
     }
 
---
+++ test_serverless.py
@@ -0,0 +1,10 @@
+import requests
+import json
+body = {
+    "optimizer": "RMSprop"
+}
+body = json.dumps(body)
+resp = requests.post("https://5xpj1pu5y8.execute-api.ap-south-1.amazonaws.com/dev/generator", data=body)
+body = resp.json()
+# print(body["input"]["body"])
+print(body["code"])
