c9997d1b6e2fe8e5e616ea1c0ec3658fd6fbef87
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2018-02-26 12:44:43 +0530

    Added More vigorous test data

--- test_data.py
+++ test_data.py
@@ -2,13 +2,17 @@ import random
 
 def random_data():
     fl = open('data.csv','w+')
-    fl.write('0,input1,input2,input3,output1,output2')
+    fl.write('0,input1,input2,output1,output2')
     fl.write('\n')
-    for i in range(1000):
+    for i in range(100):
         a = random.randint(0,10)
+        # b = a*a
         b = random.randint(0,10)
-        c = random.randint(0,10)
-        fl.write('{0},{1},{2},{3},{4},{5}'.format(i,a,b,c,a+b+c,a-b+c))
+        c = a*b
+        d = a*a
+        # c = random.randint(0,10)
+        # fl.write('{0},{1},{2},{3},{4},{5}'.format(i,a,b,c,a+b+c,a-b+c))
+        fl.write('{0},{1},{2},{3},{4}'.format(i, a, b, c, d))
         fl.write('\n')
     fl.close()
 
